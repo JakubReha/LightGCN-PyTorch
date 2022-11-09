bca96601a2f8d5a6e9ae8d92af8df4e9be8e50a5
edwardxiaoyicong <112631658+yxiaoaz@users.noreply.github.com>
2022-11-09 10:17:23 +0800

    Update baselines.py

--- code/baselines.py
+++ code/baselines.py
@@ -2,29 +2,24 @@ import os
 
 import numpy as np
 import pandas as pd
-from collections import defaultdict
-
-import surprise
-from surprise import accuracy, SVD, SVDpp
 from surprise.dataset import Reader, Dataset
-from surprise import KNNBasic
 import multiprocessing
 import world
 import utils
 import torch
-from sklearn.metrics import roc_auc_score
-import register
 from register import dataset
-
 
 CORES = multiprocessing.cpu_count() // 2
 
-#static method to generate df_train and df_test
+"""
+Static method to generate df_train and df_test.
+Will be called upon when related files are not detected in directory.
+"""
 def train_test_generate():
     if os.path.exists("../data/ml-latest-small/df_train.csv") and os.path.exists("../data/ml-latest-small/df_test.csv"):
         df_train = pd.read_csv("../data/ml-latest-small/df_train.csv")
         df_test = pd.read_csv("../data/ml-latest-small/df_test.csv")
-        return df_train,df_test
+        return df_train, df_test
     movies = pd.read_csv("../data/ml-latest-small/movies.csv")
     df = pd.read_csv("../data/ml-latest-small/ratings.csv")
     movies["movieId"] = movies["movieId"] - 1
@@ -38,16 +33,20 @@ def train_test_generate():
     df_train = df.iloc[train_index, :][["userId", "movieId", "rating"]]
     df_test = df.iloc[test_index, :][["userId", "movieId", "rating"]]
 
-    return df_train,df_test
+    return df_train, df_test
 
+"""
+The tailored engine for all baseline models that we use. 
+Only supports models imported from scikit-surprise package.
+To run exxperiments with baseline models, run baseline_experiments.py
+"""
 class baseLineModel():
 
-    def __init__(self,model):
-        self.model=model;
-        self.df_train,self.df_test=train_test_generate()
-        #print(self.cls)
+    def __init__(self, model):
+        self.model = model;
+        self.df_train, self.df_test = train_test_generate()
 
-    def test_one_batch(self,X):
+    def test_one_batch(self, X):
         sorted_items = X[0].numpy()
         groundTrue = X[1]
         r = utils.getLabel(groundTrue, sorted_items)
@@ -61,7 +60,7 @@ class baseLineModel():
                 'precision': np.array(pre),
                 'ndcg': np.array(ndcg)}
 
-    def Test(self,dataset, model, multicore=0):
+    def Test(self, dataset, model, multicore=0):
         u_batch_size = world.config['test_u_batch_size']
         dataset: utils.BasicDataset
         testDict: dict = dataset.testDict
@@ -81,8 +80,6 @@ class baseLineModel():
             users_list = []
             rating_list = []
             groundTrue_list = []
-            # auc_record = []
-            # ratings = []
             total_batch = int(np.ceil(len(users) / u_batch_size))
             for batch_users in utils.minibatch(users, batch_size=u_batch_size):
                 allPos = dataset.getUserPosItems(batch_users)
@@ -90,12 +87,10 @@ class baseLineModel():
                 batch_users_gpu = torch.Tensor(batch_users).long()
                 batch_users_gpu = batch_users_gpu.to(world.device)
 
-                # rating = model.getUsersRating(batch_users_gpu)
-                rating = torch.zeros((len(batch_users_gpu), dataset.UserItemNet.shape[1]),dtype=torch.float)
+                rating = torch.zeros((len(batch_users_gpu), dataset.UserItemNet.shape[1]), dtype=torch.float)
                 for i in range(len(batch_users)):
                     for j in range(dataset.UserItemNet.shape[1]):
                         rating[i, j] = model.predict(uid=i, iid=j).est
-                # rating = rating.cpu()
                 exclude_index = []
                 exclude_items = []
                 for range_i, items in enumerate(allPos):
@@ -104,12 +99,6 @@ class baseLineModel():
                 rating[exclude_index, exclude_items] = -(1 << 10)
                 _, rating_K = torch.topk(rating, k=max_K)
                 rating = rating.cpu().numpy()
-                # aucs = [
-                #         utils.AUC(rating[i],
-                #                   dataset,
-                #                   test_data) for i, test_data in enumerate(groundTrue)
-                #     ]
-                # auc_record.extend(aucs)
                 del rating
                 users_list.append(batch_users)
                 rating_list.append(rating_K.cpu())
@@ -122,7 +111,7 @@ class baseLineModel():
                 pre_results = []
                 for x in X:
                     pre_results.append(self.test_one_batch(x))
-            scale = float(u_batch_size / len(users))
+            float(u_batch_size / len(users))
             for result in pre_results:
                 results['recall'] += result['recall']
                 results['precision'] += result['precision']
@@ -130,52 +119,13 @@ class baseLineModel():
             results['recall'] /= float(len(users))
             results['precision'] /= float(len(users))
             results['ndcg'] /= float(len(users))
-            # results['auc'] = np.mean(auc_record)
             if multicore == 1:
                 pool.close()
             print(results)
             return results
 
-    def precision_recall_at_k(self,predictions, k=20, threshold=3.5):
-        """Return precision and recall at k metrics for each user"""
-        # First map the predictions to each user.
-        user_est_true = defaultdict(list)
-        for uid, _, true_r, est, _ in predictions:
-            user_est_true[uid].append((est, true_r))
-
-        precisions = dict()
-        recalls = dict()
-        for uid, user_ratings in user_est_true.items():
-            # Sort user ratings by estimated value
-            user_ratings.sort(key=lambda x: x[0], reverse=True)
-
-            # Number of relevant items
-            n_rel = sum((true_r >= threshold) for (_, true_r) in user_ratings)
-
-            # Number of recommended items in top k
-            n_rec_k = sum((est >= threshold) for (est, _) in user_ratings[:k])
-
-            # Number of relevant and recommended items in top k
-            n_rel_and_rec_k = sum(
-                ((true_r >= threshold) and (est >= threshold))
-                for (est, true_r) in user_ratings[:k]
-            )
-
-            # Precision@K: Proportion of recommended items that are relevant
-            # When n_rec_k is 0 Precision is undefined. We here set it to 0.
-
-            precisions[uid] = n_rel_and_rec_k / n_rec_k if n_rec_k != 0 else 0
-
-            # Recall@K: Proportion of relevant items that are recommended
-            # When n_rel is 0 Recall is undefined. We here set it to 0.
-
-            recalls[uid] = n_rel_and_rec_k / n_rel if n_rel != 0 else 0
-
-        return precisions, recalls
-
     def run(self):
         reader = Reader(rating_scale=(0.5, 5.0))
-
 
         ratings_df_train = pd.DataFrame(
             {'userID': self.df_train['userId'].astype(int), 'movieID': self.df_train['movieId'].astype(int),
@@ -191,45 +141,4 @@ class baseLineModel():
         data_test_surprise = Dataset.load_from_df(ratings_df_test, reader)
         data_test_surprise = data_test_surprise.build_full_trainset().build_testset()
         self.model.fit(data_train_surprise)
-        predictions = self.model.test(data_test_surprise)
-        #precisions, recalls = self.precision_recall_at_k(predictions, k=20, threshold=3.5)
-        # Precision and recall can then be averaged over all users
-        #print("Precision: "+str(sum(prec for prec in precisions.values()) / len(precisions)))
-        #print("Recall: "+str(sum(rec for rec in recalls.values()) / len(recalls)))
-        self.Test(dataset, self.model, world.config['multicore'])
-
-
-'''
-similarity_measures=["cosine","msd","pearson","pearson_baseline"]
-
-'''
-'''
-similarity_measures=["cosine","msd","pearson","pearson_baseline"]
-KNNmodels=[]
-for name in similarity_measures:
-    sim_options = {
-        "name": name,
-        "user_based": True
-    }
-    KNNmodels.append(KNNBasic(sim_options=sim_options))
-
-models={"SVD":[SVD()],
-        "KNN":KNNmodels}
-
-for model_type in models.keys():
-    for model in models[model_type]:
-        baseLineModel(model).run()
-'''
-
-sim_options = {
-            "name": "cosine",
-            "user_based": True
-        }
-model = KNNBasic(sim_options=sim_options)
-baseLineModel(model).run()
-
-
-
-
-
-
+        return self.Test(dataset, self.model, world.config['multicore'])
